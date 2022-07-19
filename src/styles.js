f44d306508a3d193bb772b7db0064814e0d6b5db
visetin <cheemeburmbger@gmail.com>
2022-07-19 18:01:28 +0300

    fix: change styles order imports

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 import 'normalize.css';
 
 import '@styles/fonts.scss';
-import '@styles/tech.scss';
 import '@styles/sitemap.scss';
+import '@styles/tech.scss';
 import '@styles/layout.scss';
 import '@styles/icons.scss';
